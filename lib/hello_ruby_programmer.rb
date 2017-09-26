69219efa760835d6df1e7edaabc6912d49c245a7
Natasha Holmes <natashanholmes@gmail.com>
2017-09-26 14:19:46 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,4 +1,4 @@
 def gretting(name)
-puts "Hi [{name}]! Welcome to the wonderful world of Ruby programming."
+puts "Hi {name}! Welcome to the wonderful world of Ruby programming."
 puts "Please enter your name so that we can greet you more personally:"
 end
